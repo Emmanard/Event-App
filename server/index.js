9c8a2220c6d564abd5d4117baa69ac9b3f74e83d
[Emmanard] <Emmanard9@gmail.com>
2025-10-15 19:44:04 +0100

    update to test backend on vercel

--- server/index.js
+++ server/index.js
@@ -17,11 +17,18 @@ const paymentRoutes = require('./routes/paymentRoutes');
 const port = process.env.PORT || 5000;
 connectDB();
 
+
+
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
 app.use(express.static("public"));
+
+
+app.get('/api/health', (req, res) => {
+  res.send('✅ EventWave backend is live on Vercel!');
+});
 
 // ✅ API key middleware
 app.use(async (req, res, next) => {
